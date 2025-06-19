80040c50493a254abc1732228f0085206f5763e7
Chaitanya Singh <157046183+CSingh26@users.noreply.github.com>
2025-06-19 02:13:39 -0700

    Initialized SwiftUI habit tracker app

---
+++ HabitTracker/HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Chaitanya Singh on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
