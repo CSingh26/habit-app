e432efd31cb7bae745c45501a363396b5b0b691c
Chaitanya Singh <157046183+CSingh26@users.noreply.github.com>
2025-06-19 10:54:49 -0700

    Basic Landing Pages Setup

---
+++ HabitTracker/HabitTracker/Views/LoginView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Login Screen")
+            .font(.title)
+            .padding()
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ HabitTracker/HabitTracker/Views/SignupView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct SignupView: View {
+    var body: some View {
+        Text("Signup Screen")
+            .font(.title)
+            .padding()
+    }
+}
+
+#Preview {
+    SignupView()
+}
+
